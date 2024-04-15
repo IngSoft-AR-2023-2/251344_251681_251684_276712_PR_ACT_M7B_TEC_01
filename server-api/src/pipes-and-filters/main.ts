3d8c0383bfd89786aff72149f5ebe12551d22269
Nicolas Pinto <nico@Nicolass-MacBook-Pro-2.local>
2024-04-15 08:44:00 -0300

    Changes in server-api/main.ts

--- server-api/src/pipes-and-filters/main.ts
+++ server-api/src/pipes-and-filters/main.ts
@@ -2,29 +2,28 @@
 import { QueueFactory } from './pipeline/QueueFactory';
 import { Pipeline } from './pipeline/Pipeline';
 import { toLowercaseWithSpaces, toUppercase, replaceSpacesWithDots, filterWithRandomError } from './filters/filters';
-import { CustomData } from './data-structure/CustomData';
 import fs from "fs";
+import { Client } from './data-structure/Client';
 
 // construye una funcion de creacion de colas dependiendo de un parm se crea una funcion u otra (bull o rabbit)
-const queueFactory = QueueFactory.getQueueFactory<CustomData>; //ojo que no la invoca aca si no dentro de la Pipeline
+const queueFactory = QueueFactory.getQueueFactory<Client>; //ojo que no la invoca aca si no dentro de la Pipeline
 
 // Crear una nueva instancia de Pipeline usando Bull como backend de la cola
-const pipeline = new Pipeline<CustomData>([toLowercaseWithSpaces, filterWithRandomError,toUppercase, replaceSpacesWithDots], queueFactory);
+const pipeline = new Pipeline<Client>([toLowercaseWithSpaces, filterWithRandomError,toUppercase, replaceSpacesWithDots], queueFactory);
 
 let outputs: string[] = [];
 
-pipeline.on('finalOutput', (data: CustomData) => {
-  outputs.push("Salida final: " + data.data);
+pipeline.on('finalOutput', (data: Client) => {
+  outputs.push("Salida final: " + data.nombre);
   fs.writeFileSync("outputs.txt", outputs.join("\n"));
 });
 
-pipeline.on('errorInFilter', (error: Error, data: CustomData) => {
-  outputs.push("Error en el filtro: " + error.message + " " + data.data);
+pipeline.on('errorInFilter', (error: Error, data: Client) => {
+  outputs.push("Error en el filtro: " + error.message + " " + data.nombre);
   fs.writeFileSync("outputs.txt", outputs.join("\n"));
 });
 
-export const processWords = (words: string[]) => {
-  words.map((word) => {
-    pipeline.processInput({ data: word });
-  });
+export const processClient = (client: Client) => {
+    pipeline.processInput({ nombre: client.nombre, apellido: client.apellido, cedula: client.cedula, 
+      telefono: client.telefono, departamento: client.departamento, necesita_asistencia_movilidad: client.necesita_asistencia_movilidad});
 };
